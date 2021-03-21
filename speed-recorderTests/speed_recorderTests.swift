c953d98c74346328bb8322c04b2253781bef1d8f
Ming Wang <ming.wang@ouster.io>
2021-03-20 23:24:54 -0400

    Initial Commit

---
+++ speed-recorderTests/speed_recorderTests.swift
@@ -0,0 +1,33 @@
+//
+//  speed_recorderTests.swift
+//  speed-recorderTests
+//
+//  Created by Ming Wang on 3/20/21.
+//
+
+import XCTest
+@testable import speed_recorder
+
+class speed_recorderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
